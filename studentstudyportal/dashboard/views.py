0b8835c8dedae512fdb5fd1e7afa29dcd6b267ee
Pradeep92hv <hvpradeep92@gmail.com>
2024-10-24 16:09:26 +0530

    working

--- studentstudyportal/dashboard/views.py
+++ studentstudyportal/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # from . forms import NotesForm
 from . form import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,7 +9,17 @@ def home(request):
 
 
 def notes(request):
-    form=NotesForm()
+    if request.method == "POST":
+        form = NotesForm(request.POST)
+        
+        if form.is_valid():
+            notes =Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+            
+        messages.success(request,f"notes added from {request.user.username} Successfully")
+        
+    else:
+        form=NotesForm()
     notes = Notes.objects.filter(user=request.user)
     
     context = { 'notes' :notes,'form': form}
