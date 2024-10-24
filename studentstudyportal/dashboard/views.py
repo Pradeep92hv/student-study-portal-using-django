31eeeee804cedf4625fd38bb4f8a13e0021602a6
Pradeep92hv <hvpradeep92@gmail.com>
2024-10-24 14:44:03 +0530

    created form

--- studentstudyportal/dashboard/views.py
+++ studentstudyportal/dashboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# from . forms import NotesForm
+from . form import *
 
 # Create your views here.
 def home(request):
@@ -6,4 +8,8 @@ def home(request):
 
 
 def notes(request):
-    return render(request,'dashboard/notes.html')
+    form=NotesForm()
+    notes = Notes.objects.filter(user=request.user)
+    
+    context = { 'notes' :notes,'form': form}
+    return render(request,'dashboard/notes.html',context)
